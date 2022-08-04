6feed3eb09f5ebb83957d6dd30b85edb3fd22ff0
Shane Wilson <shanezilla@gmail.com>
2022-08-03 21:13:31 -0400

    add muuntaja for json

--- src/main/zaal/pedestal.clj
+++ src/main/zaal/pedestal.clj
@@ -1,17 +1,68 @@
 (ns zaal.pedestal
   (:require [integrant.core :as ig]
-            [io.pedestal.http :as http]
-            [io.pedestal.http.body-params :as body-params]
-            [io.pedestal.http.route :as route]))
+            [io.pedestal.http.route :as route]
+            [muuntaja.interceptor :as muuntaja]
+            [zaal.pathom :as pathom]))
 
-(defn respond-hello [_]
-  {:status 200 :body "Hello, world!"})
+(def unmentionables #{"YHWH" "Voldemort" "Mxyzptlk" "Rumplestiltskin" "曹操"})
 
-(def common-interceptors [(body-params/body-params) http/json-body])
+(defn response [status body & {:as headers}]
+  {:status status :body body :headers headers})
+
+(def ok       (partial response 200))
+(def created  (partial response 201))
+(def accepted (partial response 202))
+
+(defn not-found []
+  {:status 404 :body "Not found\n"})
+
+(defn greeting-for [greeting name]
+  (cond
+    (unmentionables name) nil
+    (empty? name)         "Hello, world!\n"
+    :else               {:response (str greeting " ! " name)}))
+
+(defn respond-hello [request]
+  (let [name   (get-in request [:path-params :name])
+        greeting   (get-in request [:query-params :name])
+        resp (greeting-for greeting name)]
+    (if resp
+      (ok (pathom/pathom [:acme.math/pi]))
+      (not-found))))
+
+(def echo
+  {:name :echo
+   :enter
+   (fn [context]
+     (let [request (:request context)
+           response (let [name   (get-in request [:path-params :name])
+                          qparams   (get-in request [:query-params])
+                          pparams   (get-in request [:path-params])
+                          bparams   (get-in request [:body-params])
+                          params   (get-in request [:params])
+                          greeting   (get-in request [:query-params :name])
+                          resp (greeting-for greeting name)]
+                      (if resp
+                        (ok {:params params :qparams qparams :bparams bparams :pparams pparams})
+                        (not-found)))]
+       (assoc context :response response)))})
+
+(def graph
+  {:name :graph
+   :enter (fn [context]
+            (let [request (:request context)
+                  query (get-in request [:params :query])
+
+                  response (pathom/pathom query)]
+
+              (assoc context :response (ok request))))})
+
 (defn routes [_]
   (route/expand-routes
-   #{["/greet" :get (conj common-interceptors `respond-hello) :route-name :greet]
-     ["/recipes" :get respond-hello :route-name :recipes]}))
+   #{["/greet" :get respond-hello :route-name :greet]
+     ["/greet/:name" :post [(muuntaja/format-interceptor) (muuntaja/params-interceptor) echo] :route-name :greet-name]
+     ["/graph" :post graph :route-name :graph]}))
+
 
 (defn app
   [config]

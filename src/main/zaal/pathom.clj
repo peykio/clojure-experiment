b7aa24f50028a30f315cd1a60b96424377dd279e
Shane Wilson <shanezilla@gmail.com>
2022-08-05 21:13:34 -0400

    connect datomic db into pathom env

--- src/main/zaal/pathom.clj
+++ src/main/zaal/pathom.clj
@@ -1,14 +1,11 @@
 (ns zaal.pathom
-  (:require
-   [cognitect.transit :as t]
-   [muuntaja.core :as muuntaja]
-   [com.wsscode.pathom3.connect.indexes :as pci]
-   [com.wsscode.pathom3.connect.operation :as pco]
-   [com.wsscode.pathom3.connect.planner :as pcp]
-   [com.wsscode.pathom3.connect.operation.transit :as pcot]
-   [com.wsscode.pathom3.interface.eql :as p.eql]
-
-   [zaal.middleware :as mw]))
+  (:require [com.wsscode.pathom3.connect.indexes :as pci]
+            [com.wsscode.pathom3.connect.operation :as pco]
+            [com.wsscode.pathom3.connect.planner :as pcp]
+            [com.wsscode.pathom3.connect.built-in.plugins :as pbip]
+            [com.wsscode.pathom3.plugin :as p.plugin]
+            [datomic.client.api :as d]
+            [integrant.core :as ig]))
 
 ; create a var to store the cache
 (defonce plan-cache* (atom {}))
@@ -39,48 +36,46 @@
   (get todo-db id))
 
 (pco/defresolver constant-pi []
-  {:acme.math/pi 3.1415})
-(def env
+  {:acme.math/pi 3.14})
+
+(pco/defresolver list-recipes [env _]
+  {::pco/output
+   [{:app/all-recipes
+     [:recipe/recipe-id]}]}
+  {:app/all-recipes (d/q '[:find (pull ?e [:recipe/recipe-id])
+                           :where [?e :recipe/public? true]]
+                         (:db env))})
+
+(pco/defresolver get-recipe [env {:recipe/keys [id]}]
+  {::pco/output
+   [:recipe/recipe-id
+    :recipe/prep-time
+    :recipe/display-name
+    :recipe/image-url
+    :recipe/public?]}
+  (d/q '[:find (pull ?e [:recipe/recipe-id
+                         :recipe/prep-time
+                         :recipe/display-name
+                         :recipe/image-url
+                         :recipe/public?])
+         :in $ id
+         :where [?e :recipe/recipe-id id]]
+       (:db env) id))
+
+
+(defn env [{:keys [conn]}]
   ; persistent plan cache
   (-> {::pcp/plan-cache* plan-cache*}
-      (pci/register
-       [constant-pi
-        todo-items
-        todo-by-id])))
-(def pathom (p.eql/boundary-interface env))
+      (pci/register [constant-pi
+                     todo-items
+                     todo-by-id
+                     list-recipes
+                     get-recipe])
+      (p.plugin/register [(pbip/env-wrap-plugin #(assoc % :db (d/db conn)))])))
 
-(def muuntaja-options
-  (update-in
-   muuntaja/default-options
-   [:formats "application/transit+json"]
-    ; in this part we setup the read and write handlers for Pathom resolvers and mutations
-   merge {:decoder-opts {:handlers pcot/read-handlers}
-          :encoder-opts {:handlers  pcot/write-handlers
-                          ; write-meta is required if you wanna see execution stats on Pathom Viz
-                         :transform t/write-meta}}))
+(defmethod ig/init-key ::env
+  [_ {:keys [datomic]}]
+  (println datomic)
+  (println "Pathom Env")
+  (env datomic))
 
-(defn pathom-handler [{:keys [body-params]}]
-  {:status 200
-   :body   (pathom body-params)})
-
-(def routes
-  ["/pathom" {:swagger {:tags ["pathom"]}
-              :middleware [[mw/wrap-auth0]
-                        ;;    [(middleware/wrap-format muuntaja-options)]
-                           ]}
-   [""
-    {:get {:handler pathom-handler
-           :responses {200 {:body any?}}
-           :summary "Pathom"}
-     :post {:handler pathom-handler
-            :responses {200 {:body any?}}
-            :summary "Pathom"}}]])
-
-
-
-(comment
-  ; WARNING: this will trigger a lot of requests, may take some time. Also, HN may
-  ; throttle or block some accesses from you.
-  (p.eql/process env
-                 [:acme.math/pi])
-  (pathom [:acme.math/pi]))

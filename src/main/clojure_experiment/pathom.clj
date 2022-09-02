3305bc2717af08e02900c8f12640c32731b5ff35
Shane Wilson <shanezilla@gmail.com>
2022-09-02 16:45:50 -0400

    ✨ use kaocha to auto gen tests from specs
    kaocha seems to be the goto test runner these days. it has built in integration with spec/gen where it can actually run generative tests for all specs it finds without needing to setup them up in a test file or use stest/check.
    
    also cleans up malli and old libs

--- src/main/clojure_experiment/pathom.clj
+++ src/main/clojure_experiment/pathom.clj
@@ -9,29 +9,13 @@
             [com.wsscode.misc.coll :as coll]
             [datomic.client.api :as d]
             [integrant.core :as ig]
-            [malli.core :as m]
-            [malli.transform :as mt]
-            [malli.error :as me]
             [clojure.spec.alpha :as s]
-            [clojure.spec.gen.alpha :as g]
             [exoscale.coax :as c]
             [exoscale.coax.coercer :as cc]))
 
 
-(def clamp-to-range-transformer
-  {:name :clamp-to-range
-   :compile (fn
-              [schema _]
-              (let [vmin (:min (m/properties schema) java.lang.Integer/MIN_VALUE)
-                    vmax (:max (m/properties schema) java.lang.Integer/MAX_VALUE)]
-                (fn [v] (-> v (max vmin) (min vmax)))))})
 
 (def LIMIT_MAX 100)
-(def ?limit [:int {:min 0 :max LIMIT_MAX :default 10 :decode/clamp-to-range clamp-to-range-transformer}])
-(def ?offset [:int {:min 0 :default 0 :decode/clamp-to-range clamp-to-range-transformer}])
-(def ?pagination [:map {:closed true}
-                  [:limit ?limit]
-                  [:offset ?offset]])
 (defn coercer-with-default [coercer default x]
   (let [v (coercer x nil)]
     (if (= v :exoscale.coax/invalid)
@@ -43,10 +27,10 @@
         vmax (:max opts java.lang.Integer/MAX_VALUE)]
     (-> x (max vmin) (min vmax))))
 
-(s/def ::limit (s/int-in 0 101))
+(s/def ::limit (s/int-in 0 (inc LIMIT_MAX)))
 (c/def ::limit (fn [x _] (->> x
                               (coercer-with-default cc/to-long 10)
-                              (clamp {:min 0 :max 100})
+                              (clamp {:min 0 :max LIMIT_MAX})
                               (.longValue))))
 
 (s/def ::offset nat-int?)
@@ -57,69 +41,19 @@
 (s/def ::pagination (s/keys :req-un [::limit ::offset]))
 (s/def ::params (s/keys :opt-un [::limit ::offset]))
 
-
-
-(comment
-  (-> (s/keys :opt-un [::limit ::offset])
-      (s/with-gen #(s/gen (s/every-kv
-                           (s/or :string string? :keyword keyword? :limit #{:limit} :offset #{:offset})
-                           (s/or :string string? :int int?))))
-      s/gen
-      g/generate)
-  (m/decode ?pagination
-            {:limit "122"}
-            (mt/transformer
-             mt/strip-extra-keys-transformer
-             mt/default-value-transformer
-            ;;  mt/string-transformer
-             clamp-to-range-transformer))
-
-
-  (s/conform ::limit -11)
-  (s/explain-data ::limit "-11")
-  (c/coerce ::limit nil)
-  (c/coerce ::limit "11")
-  (c/coerce ::limit "-11")
-  (c/coerce ::limit "0")
-  (c/coerce ::limit "50")
-  (c/coerce ::limit "550")
-  (s/conform ::offset -11)
-  (c/coerce ::offset "-11")
-  (c/coerce ::offset [-11])
-  (s/explain-data ::pagination {::limit 122 ::offset [111]})
-  (c/coerce ::pagination {:limit "-"}))
-
 (s/fdef coax-pagination-params
   :args (s/cat :params (s/every-kv
                         #{:limit :offset :extra-key}
                         any?))
-  :ret  #(s/valid? ::pagination %))
-(defn coax-pagination-params [{:keys [limit offset]}] (c/coerce ::pagination {:limit limit :offset offset}))
-
-(s/fdef decode-pagination-params
-  :args (s/cat :params (s/every-kv
-                        #{:limit :offset :extra-key}
-                        (s/or :string string? :int int?)))
-  :ret  #(s/valid? ::pagination %))
-(defn decode-pagination-params [params] (m/decode ?pagination params (mt/transformer
-                                                                      mt/strip-extra-keys-transformer
-                                                                      mt/default-value-transformer
-                                                                      mt/string-transformer
-                                                                      clamp-to-range-transformer)))
-
-(defn corce-pagination-params [params]
-  (let [ps  (m/decode ?pagination params (mt/transformer
-                                          mt/strip-extra-keys-transformer
-                                          mt/default-value-transformer
-                                          mt/string-transformer))]
-    (if (m/validate ?pagination ps)
-      (assoc params :pagination ps)
-      (throw (ex-info (str (me/humanize (m/explain ?pagination ps))) {})))))
+  :ret  ::pagination)
+(defn coax-pagination-params
+  [{:keys [limit offset]}]
+  (c/coerce ::pagination {:limit limit :offset offset}))
 
 (defn get-params [env]
   (-> env
       pco/params
-      corce-pagination-params))
+      coax-pagination-params))
 
 
 (defn get-lookup-params [lookup env]
@@ -137,7 +71,7 @@
 (defn lookup-pagination
   "The results of nested resolvers are grouped by the parent id using datalog pull syntax instead of being returned directly. This helps Pathom match results but also means we need to reach inside each returned item and set pagination values directly."
   [{:keys [lookup] :as params} e]
-  (update e lookup #(into [] (-> params :pagination pagination) %)))
+  (update e lookup #(into [] (-> params pagination) %)))
 
 (defn lookup-pipeline
   [params]
@@ -194,6 +128,7 @@
              :args [(:db env) (map :participant/participant-id items)]})
        (mapv first)
        (coll/restore-order2 items :participant/participant-id)))
+
 (pco/defresolver list-participant-specimens [env items]
   {::pco/input [:participant/participant-id]
    ::pco/output [{:participant/specimens [:specimen/specimen-id]}]
